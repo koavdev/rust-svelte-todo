b6f6e35b3e61068441a2be19a5db65b4fb59512e
koavdev <koavdev@gmail.com>
2025-07-21 23:46:59 -0300

    feat: add todos page

---
+++ frontend/src/routes/+page.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+	import type { PageData } from "./$types";
+
+	export let data: PageData;
+</script>
+<div class="container mx-auto mt-16">
+	<h1 class="h1 text-center">Todos</h1>
+
+	<form action="http://0.0.0.0:8000/create" method="POST">
+		<input class="input" name="description" type="text" placeholder="What needs to be done" />
+	</form>
+
+	{#each data.todos as todo}
+	<p>{todo.id}</p>
+	<p>{todo.description}</p>
+	<p>{todo.done}</p>
+	{/each}
+</div>
